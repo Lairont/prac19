5f12eae348fc88a73ee75cdecae4c250a11bc81a
Никита <153456515+Lairont@users.noreply.github.com>
2025-01-28 02:12:29 +0200

    Add files via upload

---
+++ ConsoleApp19/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Books
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public int Pages;
+        public void Info()
+        {
+            Console.WriteLine($"Название: {Title}\nАвтор: {Author}\nГод выпуска: {Year}\nОбъем в листах: {Pages}");
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.Title = "Евгений Онегин";
+            books.Author = "А.С.Пушкин";
+            books.Year = 1833;
+            books.Pages = 224;
+            books.Info();
+            Console.ReadLine();
+        }
+    }
+}
